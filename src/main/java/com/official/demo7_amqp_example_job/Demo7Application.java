a3af2eefdf4e9d46830ab84337ec28c28fb211cf
zhuwenjoyce <87630170a>
2020-03-23 08:09:58 +0800

    add demo 7

--- src/main/java/com/official/demo7_amqp_example_job/Demo7Application.java
+++ src/main/java/com/official/demo7_amqp_example_job/Demo7Application.java
@@ -1,4 +1,4 @@
-package com.official.demo6_footballJob;
+package com.official.demo7_amqp_example_job;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,20 +17,15 @@ import org.springframework.context.annotation.ComponentScan;
  *  我稍微改良了下
  *
  * 注意！！！job一旦@Bean生成spring实例，就会自动运行！
- *
- * 本demo注意点：每次执行前，都先执行：
- *   1、init-database-sql/schema-truncate-oracle10g.sql
- *   2、data/demo6/clear-table.sql
- *   因为会存在job不能重复启动的问题，好像不是JOB ID重复的问题，目前原因不明
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.global", "com.official.demo6_footballJob"})
-public class Demo6Application {
-    private static Logger logger = LoggerFactory.getLogger(Demo6Application.class);
+@ComponentScan(basePackages = {"com.global", "com.official.demo7_amqp_example_job"})
+public class Demo7Application {
+    private static Logger logger = LoggerFactory.getLogger(Demo7Application.class);
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Demo6Application.class, args);
-        logger.info("demo 6 SpringBoot 启动成功！");
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Demo7Application.class, args);
+        logger.info("demo 7 SpringBoot 启动成功！");
     }
 
 }
